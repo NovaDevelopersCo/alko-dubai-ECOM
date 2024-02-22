3c781360b9042d86153aab31f6c5de51d5d6633e
Zoom <egor147536987@gmail.com>
2024-02-22 19:15:37 +0300

    init

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,34 +1,37 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-        envFilePath: `.${process.env.NODE_ENV}.env`,
-        isGlobal: true,
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+      isGlobal: true,
     }),
     ServeStaticModule.forRoot({
-        rootPath: path.resolve(__dirname, 'static'),
+      rootPath: path.resolve(__dirname, 'static'),
     }),
     SequelizeModule.forRoot({
-        dialect: 'postgres',
-        host: process.env.POSTGRES_HOST,
-        port: Number(process.env.POSTGRES_PORT),
-        username: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DB,
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false,
-            },
+      dialect: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
         },
-        autoLoadModels: true,
-        synchronize: true,
+      },
+      autoLoadModels: true,
+      synchronize: true,
     }),
-],
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
