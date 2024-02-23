d24b83faf5c8785ac096c89c0ce133862c18b051
Zoom <egor147536987@gmail.com>
2024-02-23 13:16:25 +0300

    Update Order DTO and Service (generated by blackbox.ai )

--- backend/src/order/dto/create-order.dto.ts
+++ backend/src/order/dto/create-order.dto.ts
@@ -3,7 +3,20 @@ import { Items } from 'src/items/items.model';
 
 export class CreateOrderDto {
   @ApiProperty({
-    example: 'Шато Марго 1958',
+    example: [
+      {
+        id: 1,
+        title: 'testItem',
+        description: 'Старая цена',
+        image: '12345',
+        category: '12345',
+        price: 12345,
+        oldPrice: 12345,
+        viewsCount: 12345,
+        sale: true,
+        soldOut: true,
+      },
+    ],
     description: 'Название',
     required: true,
   })
@@ -36,7 +49,7 @@ export class CreateOrderDto {
   @ApiProperty({
     example: 'Белое полусухое',
     description: 'Категория',
-    required: true,
+    required: false,
   })
   readonly status: string;
 }
--- backend/src/order/order.service.ts
+++ backend/src/order/order.service.ts
@@ -5,22 +5,22 @@ import { CreateOrderDto } from './dto/create-order.dto';
 
 @Injectable()
 export class OrderService {
-  constructor(@InjectModel(Order) private itemsRepository: typeof Order) {}
+  constructor(@InjectModel(Order) private ordersRepository: typeof Order) {}
 
   async getAllItems() {
-    const orders = await this.itemsRepository.findAll({
+    const orders = await this.ordersRepository.findAll({
       include: { all: true },
     });
     return orders;
   }
 
   async create(dto: CreateOrderDto) {
-    const order = await this.itemsRepository.create({ ...dto });
+    const order = await this.ordersRepository.create(dto);
     return order;
   }
 
   async getItemById(id: number) {
-    const order = await this.itemsRepository.findOne({
+    const order = await this.ordersRepository.findOne({
       where: { id },
     });
     return order;
