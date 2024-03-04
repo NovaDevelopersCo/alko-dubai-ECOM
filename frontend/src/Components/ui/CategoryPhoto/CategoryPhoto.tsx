0e3c2f5c73a65398a0099d86176fc42c8531fac9
Zoom <egor147536987@gmail.com>
2024-03-04 21:10:18 +0300

    Add links to category pages for carousel images (generated by blackbox.ai )

--- frontend/src/Components/ui/CategoryPhoto/CategoryPhoto.tsx
+++ frontend/src/Components/ui/CategoryPhoto/CategoryPhoto.tsx
@@ -5,6 +5,7 @@ import useEmblaCarousel from 'embla-carousel-react'
 import { useAppDispatch, useAppSelector } from '@/lib/hooks'
 import { fetchCategories } from '@/lib/features/categories/categories'
 import Skeleton from '@/Components/Skeleton/Skeleton'
+import Link from 'next/link'
 type PropType = {
   slides: number[]
   options?: EmblaOptionsType
@@ -37,15 +38,18 @@ const CategoryPhoto: React.FC<PropType> = (props) => {
       >
         <div className="embla__container flex touch-action: pan-y">
           {Object.keys(categories).length > 1 ? Object.keys(categories).map((index) => (
-            <div
-              className="embla__slide min-w-0 rounded-md shadow-inset-0.2rem flex-0 items-center justify-center rounded-1.8rem flex-shrink-0 w-1/2 px-2 sm:w-1/4 lg:w-1/6 xl:w-1/8 mt-3"
-              key={index}
-            >
-              <img className="w-full h-auto rounded-md" src="https://e7.pngegg.com/pngimages/804/985/png-clipart-wine-glass-bottle-product-blueberry-hill-rose-glass-wine.png" />
-              <p className="embla__slide__number p-2 text-xs sm:text-xs md:text-sm xl:text-base font-semibold text-center">
-                {index}
-              </p>
-            </div>
+            <Link key={index} href={`/catalog/${index}`}>
+              <div
+                className="embla__slide min-w-0 rounded-md shadow-inset-0.2rem flex-0 items-center justify-center rounded-1.8rem flex-shrink-0 w-full max-w-48 px-2 mt-3 "
+                key={index}
+              >
+                <img className="w-full h-auto rounded-md" src="https://e7.pngegg.com/pngimages/804/985/png-clipart-wine-glass-bottle-product-blueberry-hill-rose-glass-wine.png" />
+                <p className="embla__slide__number p-2 text-xs sm:text-xs md:text-sm xl:text-base font-semibold text-center">
+                  {index}
+                </p>
+              </div>
+            </Link>
+
           ))
             : Array.from({ length: 6 }, (_, index) => <Skeleton key={index} />)
           }
