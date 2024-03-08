408327d57ad8349d4bb382808f9cee6e41930dd0
Zoom <egor147536987@gmail.com>
2024-03-08 15:59:39 +0300

    Update frontend code: add Next.js Link component to FooterLinks

--- frontend/src/Components/ui/Catalog/Catalog.tsx
+++ frontend/src/Components/ui/Catalog/Catalog.tsx
@@ -1,13 +1,33 @@
-import React from 'react'
+"use client"
+import React, { useEffect } from 'react'
 import { Slider, Switch } from 'antd'
+import { useAppDispatch, useAppSelector } from '@/lib/hooks'
+import { RootState } from '@/lib/store'
+import { fetchCategories } from '@/lib/features/categories/categories'
 
 export default function Catalog() {
+    const dispatch = useAppDispatch()
+    const categories = useAppSelector(
+        (state: RootState) => state.categories.posts,
+    ) as any
+    const categoriesArray = Array.from(categories) as { title: string }[]
+    const titles = categoriesArray.map((category) => category.title)
+
+    useEffect(() => {
+        dispatch(fetchCategories())
+    }, [dispatch])
     return (
-        <div className="w-80 pl-2 flex justify-center flex-col">
+        <div className="w-80 pl-6 flex justify-center flex-col mt-20">
             <p>Цена</p>
             <div className="w-52">
-                <Slider range step={100} max={12000} />
+                <Slider
+                    range
+                    defaultValue={[1000, 10000]}
+                    step={100}
+                    max={12000}
+                />
             </div>
+            <h1>Каталог</h1>
         </div>
     )
 }
