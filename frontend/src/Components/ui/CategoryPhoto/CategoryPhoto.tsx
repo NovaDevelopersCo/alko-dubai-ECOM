c3a9c3edf052eb3213ed7a3e1960d8c820ef263f
Zoom <egor147536987@gmail.com>
2024-03-08 14:17:02 +0300

    Update CategoryPhoto component to display category items count and add skeleton loading state

--- frontend/src/Components/ui/CategoryPhoto/CategoryPhoto.tsx
+++ frontend/src/Components/ui/CategoryPhoto/CategoryPhoto.tsx
@@ -5,7 +5,7 @@ import useEmblaCarousel from 'embla-carousel-react'
 import { useAppDispatch, useAppSelector } from '@/lib/hooks'
 import { fetchCategories } from '@/lib/features/categories/categories'
 import Link from 'next/link'
-import Skeleton from 'react-loading-skeleton';
+import Skeleton from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
 
 type PropType = {
@@ -25,11 +25,11 @@ const CategoryPhoto: React.FC<PropType> = (props) => {
 
   const dispatch = useAppDispatch()
   const categories = useAppSelector((state) => state.categories.posts)
+  console.log(Object.keys(categories))
 
   useEffect(() => {
     dispatch(fetchCategories())
   }, [dispatch])
-
 
   return (
     <section className="embla max-w-70rem mx-auto">
@@ -38,22 +38,31 @@ const CategoryPhoto: React.FC<PropType> = (props) => {
         ref={emblaRef}
       >
         <div className="embla__container flex touch-action: pan-y">
-          {Object.keys(categories).length > 1 ? Object.keys(categories).map((index) => (
-            <Link key={index} href={`/catalog/${index}`}>
-              <div
-                className="embla__slide min-w-0 rounded-md shadow-inset-0.2rem flex-0 items-center justify-center rounded-1.8rem flex-shrink-0 w-full max-w-48 px-2 mt-3 "
-                key={index}
-              >
-                <img className="w-full h-auto rounded-md" src="https://e7.pngegg.com/pngimages/804/985/png-clipart-wine-glass-bottle-product-blueberry-hill-rose-glass-wine.png" />
-                <p className="embla__slide__number p-2 text-xs sm:text-xs md:text-sm xl:text-base font-semibold text-center">
-                  {index}
-                </p>
-              </div>
-            </Link>
-
-          ))
-            : Array.from({ length: 6 }, (_, index) => <Skeleton width={150} height={150} className="rounded m-4" key={index}  />)
-          }
+          {Object.keys(categories).length > 1
+            ? Object.keys(categories).map((index) => (
+                <Link key={index} href={`/catalog/${index}`}>
+                  <div
+                    className="embla__slide min-w-0 rounded-md shadow-inset-0.2rem flex-0 items-center justify-center rounded-1.8rem flex-shrink-0 w-full max-w-48 px-2 mt-3 "
+                    key={index}
+                  >
+                    <img
+                      className="w-full h-auto rounded-md"
+                      src="https://e7.pngegg.com/pngimages/804/985/png-clipart-wine-glass-bottle-product-blueberry-hill-rose-glass-wine.png"
+                    />
+                    <p className="embla__slide__number p-2 text-xs sm:text-xs md:text-sm xl:text-base font-semibold text-center">
+                      {index}
+                    </p>
+                  </div>
+                </Link>
+              ))
+            : Array.from({ length: 6 }, (_, index) => (
+                <Skeleton
+                  width={150}
+                  height={150}
+                  className="rounded mr-4"
+                  key={index}
+                />
+              ))}
         </div>
       </div>
 
