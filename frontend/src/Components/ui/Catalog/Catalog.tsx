0fd783e9f3b4ea83d3b99e44ee8c6818c16fb6db
Zoom <egor147536987@gmail.com>
2024-03-08 15:54:04 +0300

    Update dependencies and add price filter UI.

--- frontend/src/Components/ui/Catalog/Catalog.tsx
+++ frontend/src/Components/ui/Catalog/Catalog.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Slider, Switch } from 'antd'
 
 export default function Catalog() {
-  return (
-    <div>Catalog</div>
-  )
+    return (
+        <div className="w-80 pl-2 flex justify-center flex-col">
+            <p>Цена</p>
+            <div className="w-52">
+                <Slider range step={100} max={12000} />
+            </div>
+        </div>
+    )
 }
