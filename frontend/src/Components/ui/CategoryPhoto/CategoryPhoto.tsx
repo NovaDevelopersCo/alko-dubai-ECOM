8a0bab618ddaa43c2a097977ba77b51df41c1f5f
Zoom <egor147536987@gmail.com>
2024-03-08 14:41:34 +0300

    Update CategoryPhoto component to display 6 skeleton placeholders when there are no titles.

--- frontend/src/Components/ui/CategoryPhoto/CategoryPhoto.tsx
+++ frontend/src/Components/ui/CategoryPhoto/CategoryPhoto.tsx
@@ -42,31 +42,35 @@ const CategoryPhoto: React.FC<PropType> = (props) => {
         ref={emblaRef}
       >
         <div className="embla__container flex touch-action: pan-y">
-          {titles.length > 1
-            ? titles.map((index) => (
-                <Link key={index} href={`/catalog/${index}`}>
-                  <div
-                    className="embla__slide min-w-0 rounded-md shadow-inset-0.2rem flex-0 items-center justify-center rounded-1.8rem flex-shrink-0 w-full max-w-48 px-2 mt-3 "
-                    key={index}
-                  >
-                    <img
-                      className="w-full h-auto rounded-md"
-                      src="https://e7.pngegg.com/pngimages/804/985/png-clipart-wine-glass-bottle-product-blueberry-hill-rose-glass-wine.png"
-                    />
-                    <p className="embla__slide__number p-2 text-xs sm:text-xs md:text-sm xl:text-base font-semibold text-center">
-                      {index}
-                    </p>
-                  </div>
-                </Link>
-              ))
-            : Array.from({ length: 6 }, (_, index) => (
+          {titles.length > 1 ? (
+            titles.map((index) => (
+              <Link key={index} href={`/catalog/${index}`}>
+                <div
+                  className="embla__slide min-w-0 rounded-md shadow-inset-0.2rem flex-0 items-center justify-center rounded-1.8rem flex-shrink-0 w-full max-w-48 mr-2 my-3 "
+                  key={index}
+                >
+                  <img
+                    className="w-full h-auto rounded-md"
+                    src="https://e7.pngegg.com/pngimages/804/985/png-clipart-wine-glass-bottle-product-blueberry-hill-rose-glass-wine.png"
+                  />
+                  <p className="embla__slide__number p-2 text-xs sm:text-xs md:text-sm xl:text-base font-semibold text-center">
+                    {index}
+                  </p>
+                </div>
+              </Link>
+            ))
+          ) : (
+            <div className='flex justify-between w-full'>
+              {Array.from({ length: 6 }, (_, index) => (
                 <Skeleton
                   width={150}
                   height={150}
-                  className="rounded mr-4"
+                  className="rounded my-3"
                   key={index}
                 />
               ))}
+            </div>
+          )}
         </div>
       </div>
 
