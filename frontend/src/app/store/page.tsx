71b7c6f6ed8f79191fbc4e613dd0db42aba39c6b
Zoom <egor147536987@gmail.com>
2024-03-09 22:06:00 +0300

    Update the sorting options UI.

--- frontend/src/app/store/page.tsx
+++ frontend/src/app/store/page.tsx
@@ -7,15 +7,28 @@ const store = () => {
         <>
             <div>
                 <hr className="border-[#D32B82]" />
-                <p className="text-xl text-center sm:text-right sm:mr-8">
-                    Сортировка по
-                    <select  name="sort-by" className='ml-2 text-[#D32B82] rounded'>
-                        <option value="asc" className='rounded'>цене (возрастание)</option>
-                        <option value="desc" className='rounded'>цене (убывание)</option>
-                        <option value="news" className='rounded'>новизне</option>
-                        <option value="popularity" className='rounded'>популярности</option>
-                    </select>
-                </p>
+                <Container>
+                    <p className="text-xl text-center sm:text-right">
+                        Сортировка по
+                        <select
+                            name="sort-by"
+                            className="ml-2 text-[#D32B82] rounded"
+                        >
+                            <option value="asc" className="rounded">
+                                цене (возрастание)
+                            </option>
+                            <option value="desc" className="rounded">
+                                цене (убывание)
+                            </option>
+                            <option value="news" className="rounded">
+                                новизне
+                            </option>
+                            <option value="popularity" className="rounded">
+                                популярности
+                            </option>
+                        </select>
+                    </p>
+                </Container>
                 <hr className="border-[#D32B82]" />
             </div>
             <Container>
