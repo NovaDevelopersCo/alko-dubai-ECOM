ff2a6549e25e00d0b0f47757e10eb422827c9099
Zoom <egor147536987@gmail.com>
2024-03-12 18:28:05 +0300

    Changed the sort options component to be responsive and added a filter button for mobile view.

--- frontend/src/Components/ui/Sort/Sort.tsx
+++ frontend/src/Components/ui/Sort/Sort.tsx
@@ -6,28 +6,31 @@ const Sort = () => {
         <div>
             <hr className="border-[#D32B82]" />
             <Container>
-                <p className="text-md text-center sm:text-right pr-2.5 rounded">
-                    <select
-                        name="sort-by"
-                        className="rounded cursor-pointer before:bg-[#D32B82]"
-                    >
-                        <option value="asc" className="rounded">
-                            Исходная сортировка
-                        </option>
-                        <option value="asc" className="rounded">
-                            Цене (возрастание)
-                        </option>
-                        <option value="desc" className="rounded">
-                            Цене (убывание)
-                        </option>
-                        <option value="news" className="rounded">
-                            Новизне
-                        </option>
-                        <option value="popularity" className="rounded">
-                            Популярности
-                        </option>
-                    </select>
-                </p>
+                <div className='flex justify-between items-center'>
+                    <p className="md:hidden w-max">Фильтр <span className='font-bold text-[#D32B82]'>{'>'}</span></p>
+                    <p className="text-md text-center w-max sm:text-right pr-2.5 rounded">
+                        <select
+                            name="sort-by"
+                            className="rounded cursor-pointer before:bg-[#D32B82]"
+                        >
+                            <option value="asc" className="rounded">
+                                Исходная сортировка
+                            </option>
+                            <option value="asc" className="rounded">
+                                Цене (возрастание)
+                            </option>
+                            <option value="desc" className="rounded">
+                                Цене (убывание)
+                            </option>
+                            <option value="news" className="rounded">
+                                Новизне
+                            </option>
+                            <option value="popularity" className="rounded">
+                                Популярности
+                            </option>
+                        </select>
+                    </p>
+                </div>
             </Container>
             <hr className="border-[#D32B82]" />
         </div>
