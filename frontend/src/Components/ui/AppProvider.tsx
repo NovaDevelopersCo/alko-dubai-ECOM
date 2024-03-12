460996c057728bf2ebf94a21e97661a0bab9d025
Zoom <egor147536987@gmail.com>
2024-03-12 19:51:52 +0300

    Update context names in AppProvider.

--- frontend/src/Components/ui/AppProvider.tsx
+++ frontend/src/Components/ui/AppProvider.tsx
@@ -2,17 +2,23 @@
 
 import { FC, PropsWithChildren, useState } from 'react'
 
-import { BurgerContext, CatalogContext, ModalContext } from '../context/AppContext'
+import {
+    BurgerContext,
+    CatalogContext,
+    ModalContext,
+} from '../context/AppContext'
 
 const AppProvider: FC<PropsWithChildren> = ({ children }) => {
     const [isOpen, setIsOpen] = useState<boolean>(false)
     const [visible, setVisible] = useState<boolean>(false)
-	const [visibleContext, setVisibleContext] = useState<boolean>(false)
+    const [visibleCatalog, setVisibleCatalog] = useState<boolean>(false)
 
     return (
         <ModalContext.Provider value={[visible, setVisible]}>
             <BurgerContext.Provider value={[isOpen, setIsOpen]}>
-                <CatalogContext.Provider value={[visibleContext, setVisibleContext]}>
+                <CatalogContext.Provider
+                    value={[visibleCatalog, setVisibleCatalog]}
+                >
                     {children}
                 </CatalogContext.Provider>
             </BurgerContext.Provider>
