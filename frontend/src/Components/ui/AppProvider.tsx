e7ac46791ff1211623bf34a7ba989d2cce0e8132
Zoom <egor147536987@gmail.com>
2024-03-12 19:51:18 +0300

    Update AppProvider to include CatalogContext

--- frontend/src/Components/ui/AppProvider.tsx
+++ frontend/src/Components/ui/AppProvider.tsx
@@ -2,19 +2,22 @@
 
 import { FC, PropsWithChildren, useState } from 'react'
 
-import { BurgerContext, ModalContext } from '../context/AppContext'
+import { BurgerContext, CatalogContext, ModalContext } from '../context/AppContext'
 
 const AppProvider: FC<PropsWithChildren> = ({ children }) => {
-	const [isOpen, setIsOpen] = useState<boolean>(false)
-	const [visible, setVisible] = useState<boolean>(false)
+    const [isOpen, setIsOpen] = useState<boolean>(false)
+    const [visible, setVisible] = useState<boolean>(false)
+	const [visibleContext, setVisibleContext] = useState<boolean>(false)
 
-	return (
-		<ModalContext.Provider value={[visible, setVisible]}>
-			<BurgerContext.Provider value={[isOpen, setIsOpen]}>
-					{children}
-			</BurgerContext.Provider>
-		</ModalContext.Provider>
-	)
+    return (
+        <ModalContext.Provider value={[visible, setVisible]}>
+            <BurgerContext.Provider value={[isOpen, setIsOpen]}>
+                <CatalogContext.Provider value={[visibleContext, setVisibleContext]}>
+                    {children}
+                </CatalogContext.Provider>
+            </BurgerContext.Provider>
+        </ModalContext.Provider>
+    )
 }
 
 export default AppProvider
