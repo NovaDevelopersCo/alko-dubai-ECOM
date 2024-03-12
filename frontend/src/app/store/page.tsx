9b4ace63c2a90582efc5e534937d4a03fdd99697
Zoom <egor147536987@gmail.com>
2024-03-12 18:14:57 +0300

    Add pagination component to the store page.

--- frontend/src/app/store/page.tsx
+++ frontend/src/app/store/page.tsx
@@ -2,6 +2,7 @@ import { Item } from '@/Components/entity/item'
 import Catalog from '@/Components/ui/Catalog/Catalog'
 import Container from '@/Components/ui/Container/Container'
 import Sort from '@/Components/ui/Sort/Sort'
+import { Pagination } from 'antd'
 import React from 'react'
 
 const store = () => {
@@ -13,6 +14,15 @@ const store = () => {
                     <Catalog />
                     <div className="w-max h-full my-6">
                         <Item />
+                        <div className="mt-6">
+                            <Pagination
+                                disabled={true}
+                                className="text-center"
+                                showSizeChanger={false}
+                                pageSize={10}
+                                total={500}
+                            />
+                        </div>
                     </div>
                 </div>
             </Container>
