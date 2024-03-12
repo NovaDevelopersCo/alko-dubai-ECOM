4973614e1e86c4e86d17ac387705bcaffc569ce3
Zoom <egor147536987@gmail.com>
2024-03-12 18:36:13 +0300

    Add useEffect hook to fetch items on component mount.

--- frontend/src/Components/entity/item.tsx
+++ frontend/src/Components/entity/item.tsx
@@ -23,6 +23,7 @@ export function Item() {
     useEffect(() => {
         dispatch(fetchItems(inputFetch))
     }, [dispatch, inputFetch])
+
     return (
         <div>
             <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
