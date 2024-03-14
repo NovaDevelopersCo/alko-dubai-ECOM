54c9aa02919742706dc331988f052150ea3f4da4
Igor Zimin <133769530+merdernoty@users.noreply.github.com>
2024-03-14 23:44:08 +0300

    fix

--- frontend/src/Components/ui/MainPageSort/MainPageSort.tsx
+++ frontend/src/Components/ui/MainPageSort/MainPageSort.tsx
@@ -1,56 +1,54 @@
 import React from 'react'
 import clsx from 'clsx'
 function MainPageSort() {
-  const underlineClasses = clsx(
-    'absolute',
-    'h-0.5',
-    'bg-customPink',
-    'bottom-0',
-    'left-0',
-    'w-0',
-    'group-hover:w-full',
-    'transition-all',
-    'duration-300',
-  )
+    const underlineClasses = clsx(
+        'absolute',
+        'h-0.5',
+        'bg-customPink',
+        'bottom-0',
+        'left-0',
+        'w-0',
+        'group-hover:w-full',
+        'transition-all',
+        'duration-300',
+    )
 
-  return (
-    <div className="text-center">
-      <h1 className="uppercase text-xl p-8">Популярные Товары</h1>
-      <div className="pointer ">
-        <span className="p-10">
-          <ul className="flex px-10 font-medium lg:flex-row lg:space-x-8 mt-0">
-            <li className="relative group">
-              <span
-                className="  px-10 lg:text-black border-b border-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 "
-                aria-current="page"
-              >
-                хит продаж
-              </span>
-              <span className={clsx(underlineClasses)}></span>
-            </li>
-            <li className="relative group">
-              <span
-                className=" px-10  lg:text-black border-b border-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 "
-                aria-current="page"
-              >
-                новинки
-              </span>
-              <span className={clsx(underlineClasses)}></span>
-            </li>
-            <li className="relative group">
-              <span
-                className=" px-10  lg:text-black border-b border-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 "
-                aria-current="page"
-              >
-                скидки
-              </span>
-              <span className={clsx(underlineClasses)}></span>
-            </li>
-          </ul>
-        </span>
-      </div>
-    </div>
-  )
+    return (
+        <div className="text-center">
+            <h1 className="uppercase text-xl md:text-2xl p-8">Популярные Товары</h1>
+            <div className="pointer flex justify-center">
+                <ul className="flex px-10 font-medium lg:flex-row lg:space-x-8 mt-0">
+                    <li className="relative group">
+                        <span
+                            className="px-3 text-sm lg:text-black border-b border-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700"
+                            aria-current="page"
+                        >
+                            хит продаж
+                        </span>
+                        <span className={clsx(underlineClasses)}></span>
+                    </li>
+                    <li className="relative group">
+                        <span
+                            className="px-3 text-sm lg:text-black border-b border-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700"
+                            aria-current="page"
+                        >
+                            новинки
+                        </span>
+                        <span className={clsx(underlineClasses)}></span>
+                    </li>
+                    <li className="relative group">
+                        <span
+                            className="px-3 text-sm lg:text-black border-b border-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700"
+                            aria-current="page"
+                        >
+                            скидки
+                        </span>
+                        <span className={clsx(underlineClasses)}></span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 export default MainPageSort
