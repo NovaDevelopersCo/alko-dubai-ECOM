b09981727039e160480ecc6c0c081a1dc9d3fa9f
Igor Zimin <133769530+merdernoty@users.noreply.github.com>
2024-03-15 15:33:10 +0300

    f

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,23 +5,23 @@ import { Item } from '@/Components/entity/item'
 import OurBenefits from '@/Components/ui/ourBenefits/OurBenefits'
 import MainPageSort from '@/Components/ui/MainPageSort/MainPageSort'
 export default function Home() {
-  const SLIDE_COUNT = 12
-  const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
+    const SLIDE_COUNT = 12
+    const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
 
-  return (
-    <>
-      <Container>
-        <Swaiper />
-        <CategoryPhoto slides={SLIDES} />
-        <div className='md:mx-16'>
-          <MainPageSort/>
-          <Item />
-        </div>
-      </Container>
-      <OurBenefits />
-      {/*<Container>*/}
-      {/*      */}
-      {/*  </Container>*/}
-    </>
-  )
+    return (
+        <>
+            <Container>
+                <Swaiper />
+                <CategoryPhoto slides={SLIDES} />
+                <div className="md:mx-16">
+                    <MainPageSort />
+                    <Item disabled={false} />
+                </div>
+            </Container>
+            <OurBenefits />
+            {/*<Container>*/}
+            {/*      */}
+            {/*  </Container>*/}
+        </>
+    )
 }
