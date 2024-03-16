ceb2ef9c354beea098f3539c36aca069426c69ce
Zoom <egor147536987@gmail.com>
2024-03-16 11:47:09 +0300

    Update payment details link in the header.

---
+++ frontend/src/app/payment-details/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+import PaymentForm from '@/Components/ui/PaymentForm/PaymentForm'
+import React from 'react'
+
+export default function page() {
+    return (
+        <div>
+            <PaymentForm />
+        </div>
+    )
+}
