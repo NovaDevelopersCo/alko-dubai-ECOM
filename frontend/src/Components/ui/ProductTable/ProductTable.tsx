ef9a6f9f693b31f2f84461ba9748a3916162dce8
Igor Zimin <133769530+merdernoty@users.noreply.github.com>
2024-03-25 01:14:18 +0300

    EmptyCart

--- frontend/src/Components/ui/ProductTable/ProductTable.tsx
+++ frontend/src/Components/ui/ProductTable/ProductTable.tsx
@@ -4,6 +4,7 @@ import { Product } from '../Product/Product'
 import { useAppSelector } from '@/lib/hooks'
 import { CartItem } from '@/type/interfaceCart'
 import React from 'react'
+import EmptyCart from './EmptyCart'
 
 export const ProductTable = () => {
     const { totalPrice, items } = useAppSelector(selectCart)
@@ -11,28 +12,29 @@ export const ProductTable = () => {
         (sum: number, item: any) => sum + item.count,
         0,
     )
-
     return (
         <div className="max-w-[858px]">
-            <div className="hidden lg:grid md:grid sm:hidden grid-cols-[4fr_1fr_1.5fr_1fr] lg:grid-cols-[4fr_1fr_1.5fr_1fr] md:grid-cols-[5fr_1fr_1.5fr_1fr] sm:grid-cols-[4fr_1fr_1.5fr_1fr] border-b-[1px] border-customPink">
-                <span className="text-center text-[18px] lg:text-[22px] md:text-[20px] sm:text-[18px] font-semibold pb-[5px]">
-                    товар
-                </span>
-                <span className="pl-[0] lg:pl-[30px] md:pl-[5px] sm:pl-[0] text-[18px] lg:text-[22px] md:text-[20px] sm:text-[18px] font-semibold">
-                    цена
-                </span>
-                <span className="text-center text-[18px] lg:text-[22px] md:text-[20px] sm:text-[18px] font-semibold pl-[0] lg:pl-[10px] md:pl-[0px] sm:pl-[0]">
-                    количество
-                </span>
-                <span className="text-end text-[18px] lg:text-[22px] md:text-[20px] sm:text-[18px] font-semibold">
-                    подытог
-                </span>
-            </div>
-            <div className="flex flex-col gap-[50px] lg:gap-[0px] md:gap-[0px] sm:gap-[50px]">
-                {items.map((obj: CartItem) => (
-                    <Product key={obj.id} {...obj} />
-                ))}
-            </div>
+            <>
+                <div className="hidden lg:grid md:grid sm:hidden grid-cols-[4fr_1fr_1.5fr_1fr] lg:grid-cols-[4fr_1fr_1.5fr_1fr] md:grid-cols-[5fr_1fr_1.5fr_1fr] sm:grid-cols-[4fr_1fr_1.5fr_1fr] border-b-[1px] border-customPink">
+                    <span className="text-center text-[18px] lg:text-[22px] md:text-[20px] sm:text-[18px] font-semibold pb-[5px]">
+                        товар
+                    </span>
+                    <span className="pl-[0] lg:pl-[30px] md:pl-[5px] sm:pl-[0] text-[18px] lg:text-[22px] md:text-[20px] sm:text-[18px] font-semibold">
+                        цена
+                    </span>
+                    <span className="text-center text-[18px] lg:text-[22px] md:text-[20px] sm:text-[18px] font-semibold pl-[0] lg:pl-[10px] md:pl-[0px] sm:pl-[0]">
+                        количество
+                    </span>
+                    <span className="text-end text-[18px] lg:text-[22px] md:text-[20px] sm:text-[18px] font-semibold">
+                        подытог
+                    </span>
+                </div>
+                <div className="flex flex-col gap-[50px] lg:gap-[0px] md:gap-[0px] sm:gap-[50px]">
+                    {items.map((obj: CartItem) => (
+                        <Product key={obj.id} {...obj} />
+                    ))}
+                </div>
+            </>
         </div>
     )
 }
--- frontend/src/app/cart/page.tsx
+++ frontend/src/app/cart/page.tsx
@@ -1,20 +1,29 @@
+"use client"
 import Container from '@/Components/ui/Container/Container'
+import EmptyCart from '@/Components/ui/ProductTable/EmptyCart'
 import { ProductTable } from '@/Components/ui/ProductTable/ProductTable'
 import { Total } from '@/Components/ui/Total/Total'
-import React from 'react';
+import { selectCart } from '@/lib/features/cart/cart'
+import { useAppSelector } from '@/lib/hooks'
+import React from 'react'
 
 const CartPage = () => {
+    const { items } = useAppSelector(selectCart)
     return (
         <div>
             <Container additionalStyles="pb-20 pt-[74px]">
-                <div className="grid lg:grid-cols-[3fr_1fr] md:grid-cols-1 gap-[10px] items-center">
-                    <span className="flex-1">
-                        <ProductTable />
-                    </span>
-                    <span className="mt-[60px] flex justify-center">
-                        <Total />
-                    </span>
-                </div>
+                {items.length === 0 ? (
+                    <EmptyCart />
+                ) : (
+                    <div className="grid lg:grid-cols-[3fr_1fr] md:grid-cols-1 gap-[10px] items-center">
+                        <span className="flex-1">
+                            <ProductTable />
+                        </span>
+                        <span className="mt-[60px] flex justify-center">
+                            <Total />
+                        </span>
+                    </div>
+                )}
             </Container>
         </div>
     )
