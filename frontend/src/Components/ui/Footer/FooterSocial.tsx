32fac1538261b9155470cc539b6d10c533c25202
Amir <turgunovamir9@gmail.com>
2024-03-25 18:22:30 +0800

    delete all stubs, add real tg, whatsapp links

--- frontend/src/Components/ui/Footer/FooterSocial.tsx
+++ frontend/src/Components/ui/Footer/FooterSocial.tsx
@@ -1,40 +1,27 @@
+import Link from 'next/link'
 import React from 'react'
 import { FaTelegram, FaViber } from 'react-icons/fa'
 import { IoLogoWhatsapp } from 'react-icons/io'
 import { SlSocialVkontakte } from 'react-icons/sl'
 function FooterSocial() {
-  return (
-    <div className="flex mt-4 sm:justify-center sm:mt-0">
-      <a
-        href="#"
-        className="text-gray-500  hover:text-white ms-5"
-      >
-        <FaViber />
-        <span className="sr-only">Viber</span>
-      </a>
-      <a
-        href="#"
-        className="text-gray-500  hover:text-white ms-5"
-      >
-        <SlSocialVkontakte style={{ fontSize: '17px' }} />
-        <span className="sr-only">Vkontakte</span>
-      </a>
-      <a
-        href="#"
-        className="text-gray-500  hover:text-white ms-5"
-      >
-        <IoLogoWhatsapp style={{ fontSize: '17px' }} />
-        <span className="sr-only">Whatsapp</span>
-      </a>
-      <a
-        href="#"
-        className="text-gray-500  hover:text-white ms-5"
-      >
-        <FaTelegram style={{ fontSize: '17px' }} />
-        <span className="sr-only">Telegram</span>
-      </a>
-    </div>
-  )
+    return (
+        <div className="flex mt-4 sm:justify-center sm:mt-0">
+            <Link
+                href="https://wa.me/qr/A4W3FM672FIRC1"
+                className="text-gray-500  hover:text-white ms-5"
+            >
+                <IoLogoWhatsapp style={{ fontSize: '17px' }} />
+                <span className="sr-only">Whatsapp</span>
+            </Link>
+            <Link
+                href="https://t.me/+79254692606"
+                className="text-gray-500  hover:text-white ms-5"
+            >
+                <FaTelegram style={{ fontSize: '17px' }} />
+                <span className="sr-only">Telegram</span>
+            </Link>
+        </div>
+    )
 }
 
 export default FooterSocial
