86b03a9f654dc79fd4aff3ae4c310b7d1b5864ed
Roma8642 <froma5361@gmail.com>
2024-03-28 19:52:32 +0200

    fix margin cart

--- frontend/src/Components/ui/ProductTable/ProductTable.tsx
+++ frontend/src/Components/ui/ProductTable/ProductTable.tsx
@@ -4,7 +4,6 @@ import { Product } from '../Product/Product'
 import { useAppSelector } from '@/lib/hooks'
 import { CartItem } from '@/type/interfaceCart'
 import React from 'react'
-import EmptyCart from './EmptyCart'
 
 export const ProductTable = () => {
     const { totalPrice, items } = useAppSelector(selectCart)
@@ -15,21 +14,13 @@ export const ProductTable = () => {
     return (
         <div className="max-w-[858px]">
             <>
-                <div className="hidden lg:grid md:grid sm:hidden grid-cols-[4fr_1fr_1.5fr_1fr] lg:grid-cols-[4fr_1fr_1.5fr_1fr] md:grid-cols-[5fr_1fr_1.5fr_1fr] sm:grid-cols-[4fr_1fr_1.5fr_1fr] border-b-[1px] border-customPink">
-                    <span className="text-center text-[18px] lg:text-[22px] md:text-[20px] sm:text-[18px] font-semibold pb-[5px]">
-                        товар
-                    </span>
-                    <span className="pl-[0] lg:pl-[30px] md:pl-[5px] sm:pl-[0] text-[18px] lg:text-[22px] md:text-[20px] sm:text-[18px] font-semibold">
-                        цена
-                    </span>
-                    <span className="text-center text-[18px] lg:text-[22px] md:text-[20px] sm:text-[18px] font-semibold pl-[0] lg:pl-[10px] md:pl-[0px] sm:pl-[0]">
-                        количество
-                    </span>
-                    <span className="text-end text-[18px] lg:text-[22px] md:text-[20px] sm:text-[18px] font-semibold">
-                        подытог
-                    </span>
+                <div className=" border-b-2 border-customPink hidden sm:flex">
+                    <span className="w-[25%] text-center text-lg font-semibold py-2 ml-11">товар</span>
+                    <span className="w-[25%] text-center text-lg font-semibold py-2 ml-11">цена</span>
+                    <span className="w-[25%] text-center text-lg font-semibold py-2 ml-14">количество</span>
+                    <span className="w-[25%] text-right text-lg font-semibold py-2">подытог</span>
                 </div>
-                <div className="flex flex-col gap-[50px] lg:gap-[0px] md:gap-[0px] sm:gap-[50px]">
+                <div className="flex flex-col gap-10">
                     {items.map((obj: CartItem) => (
                         <Product key={obj.id} {...obj} />
                     ))}
